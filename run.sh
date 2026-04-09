acd99a6e2574d93780f940fad281833d4e2d13a5
polandcz-cloud <polandcz@gmail.com>
2026-04-09 09:30:06 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-PORT=$(bashio::config 'port')
-export DATA_DIR="/config"
-export PORT="${PORT}"
-bashio::log.info "Starting FakturaApp v2 on port ${PORT}..."
-cd /app
-exec node server.js
